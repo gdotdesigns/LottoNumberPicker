eb77c13530a547d2f0495cacb8564b6474b45f50
Graden <toddc30@gmail.com>
2019-06-21 14:40:11 -0500

    LottoNumberPicker V1

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,30 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Picking your lottery numbers now!!!");
         int intArray[] = new int[6];
         boolean gotone = false;
         Random randnumber = new Random();
-        int index ;
+        int index;
 
 
-    System.out.println("Picking your lottery numbers now!!!");
-    for(index = 0; index <= 5; index++);
-        {
+    for(index = 0; index <= 5; index++) {
+
             while (gotone == false){
-
                 for (int num1 = 1; num1 <= 70; num1++){
                     int num2 = randnumber.nextInt(71);
                     if(num1==num2){
                         gotone = true;
                         intArray[index] = num2;
-
                     }
-
-
-
                 }
 
             }
+
         }
 
-        System.out.println(intArray);
+        for(int a=0;a<=5;a++)
+            System.out.println(intArray[a]);
 
     }
 }
