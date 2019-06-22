16bf4667e08d54099624dbc7a3bec6890d17bb93
Graden <toddc30@gmail.com>
2019-06-21 19:14:55 -0500

    LottoNumberPicker V1

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         int intArray[] = new int[6];
-        boolean gotone;
+        boolean matchFound;
         Random randnumber = new Random();
         int index;
 
 
     for(index = 0; index <= 5; index++) {
-        gotone = false;
+        matchFound = false;
         if (index < 5) {
-            while (gotone == false) {
+            while (!matchFound) {
                 for (int num1 = 1; num1 <= 70; num1++) {
                     int num2 = randnumber.nextInt(71);
                     //System.out.println(num1 + " " + num2);
                     if (num1 == num2 && IntStream.of(intArray).noneMatch(n -> n == num2)) {
                         System.out.println("SUCCESS!!!!!!!!!!!!!!!!");
                         System.out.println(num1 + " " + num2);
-                        gotone = true;
+                        matchFound = true;
                         intArray[index] = num2;
                         break;
                 }else if (num1 == num2) System.out.println("Duplicate: " + num1 + " " + num2);
