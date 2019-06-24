440bf798f74e2ccffa6c2edefd9aabe0af3e6b58
Graden <toddc30@gmail.com>
2019-06-23 19:53:21 -0500

    LottoNumberPicker V1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -8,37 +9,49 @@ public class Main {
         boolean matchFound;
         Random randomnumber = new Random();
         int index;
+        int totalBalls = 45;
+        int loops = 0;
+        int count = 1000000;
 
 
+        while (loops <= count) {
 
-        for(index = 0; index <= 5; index++) {
-        matchFound = false;
-        if (index < 5) {
-            while (!matchFound) {
-                for (int num1 = 1; num1 <= 70; num1++) {
-                    int num2 = randomnumber.nextInt(71);
-                    if (num1 == num2 && IntStream.of(intArray).noneMatch(n -> n == num2)) {
-                        matchFound = true;
-                        intArray[index] = num2;
-                        break;
-                }else if (num1 == num2) System.out.println("Duplicate: " + num1 + " " + num2);
-            }
+            for (index = 0; index <= 5; index++) {
+                matchFound = false;
+                if (index < 5) {
+                    while (!matchFound) {
+                        for (int num1 = 1; num1 <= totalBalls; num1++) {
+                            int num2 = randomnumber.nextInt(totalBalls + 1);
+                            if (num1 == num2 && IntStream.of(intArray).noneMatch(n -> n == num2)) {
+                                matchFound = true;
+                                intArray[index] = num2;
+                                break;
+                            } //else if (num1 == num2) System.out.println("Duplicate: " + num1 + " " + num2);
+                        }
 
-        }
-    }
-        else{
-            while(!matchFound) {
-                for (int num1 = 1; num1 <= 25; num1++) {
-                    int num2 = randomnumber.nextInt(26);
-                    if (num1 == num2) {
-                        intArray[index] = num2;
-                        matchFound = true;
+                    }
+                } else {
+                    while (!matchFound) {
+                        for (int num1 = 1; num1 <= 25; num1++) {
+                            int num2 = randomnumber.
+                                    nextInt(26);
+                            if (num1 == num2) {
+                                //intArray[index] = num2;
+                                matchFound = true;
+                            }
+                        }
                     }
                 }
+
             }
-        }
+            System.out.println(loops);
+            if(loops < count - 1) {
+                Arrays.fill(intArray,0);
+            }
+            loops++;
 
         }
+
         System.out.println("Picking your lottery numbers now!!!");
         for(int a=0;a<=5;a++)
             System.out.println(intArray[a]);
